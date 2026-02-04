205c1e44f26b5041a0119e2f88918f7fc5842ceb
Cristina Molina <202306116@alu.comillas.edu>
2026-02-04 11:33:11 +0100

    inicializo proyecto final

---
+++ Proyecto_PAT/src/main/java/edu/comillas/icai/gitt/pat/spring/Proyecto_PAT/ProyectoPatApplication.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.gitt.pat.spring.Proyecto_PAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPatApplication.class, args);
+	}
+
+}
---
+++ Proyecto_PAT/src/test/java/edu/comillas/icai/gitt/pat/spring/Proyecto_PAT/ProyectoPatApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.gitt.pat.spring.Proyecto_PAT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
