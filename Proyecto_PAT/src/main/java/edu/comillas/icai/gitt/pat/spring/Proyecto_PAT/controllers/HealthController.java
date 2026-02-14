2be564243fa6235257fe1733d91a380ce9f3c3df
Carla Rodriguez <202308614@alu.comillas.edu>
2026-02-14 11:55:14 +0100

    unido

--- Proyecto_PAT/src/main/java/edu/comillas/icai/gitt/pat/spring/Proyecto_PAT/controllers/HealthController.java
+++ Proyecto_PAT/src/main/java/edu/comillas/icai/gitt/pat/spring/Proyecto_PAT/controllers/HealthController.java
@@ -1,4 +1,13 @@
 package edu.comillas.icai.gitt.pat.spring.Proyecto_PAT.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
+
+    @GetMapping("/pistaPadel/health")
+    public String health() {
+        return "ok";
+    }
 }
