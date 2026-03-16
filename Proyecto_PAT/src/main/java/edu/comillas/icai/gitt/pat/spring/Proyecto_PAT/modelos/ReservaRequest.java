95e6245241978378a0d0b12b40d46208d8df0a07
Carla Rodriguez <202308614@alu.comillas.edu>
2026-03-16 23:58:00 +0100

    añadir validaciones al dto de reservas

--- Proyecto_PAT/src/main/java/edu/comillas/icai/gitt/pat/spring/Proyecto_PAT/modelos/ReservaRequest.java
+++ Proyecto_PAT/src/main/java/edu/comillas/icai/gitt/pat/spring/Proyecto_PAT/modelos/ReservaRequest.java
@@ -1,11 +1,59 @@
 package edu.comillas.icai.gitt.pat.spring.Proyecto_PAT.modelos;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class ReservaRequest {
-    public Integer idPista;
-    public LocalDate fechaReserva;
-    public LocalTime horaInicio;
-    public int duracionMinutos;
+
+    @NotNull
+    private Long idPista;
+
+    @NotNull
+    private LocalDate fechaReserva;
+
+    @NotNull
+    private LocalTime horaInicio;
+
+    //Mínimo 30 minutos
+    @Min(30)
+
+    //Máximo 180 minutos
+    @Max(180)
+    private int duracionMinutos;
+
+    public Long getIdPista() {
+        return idPista;
+    }
+
+    public void setIdPista(Long idPista) {
+        this.idPista = idPista;
+    }
+
+    public LocalDate getFechaReserva() {
+        return fechaReserva;
+    }
+
+    public void setFechaReserva(LocalDate fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public int getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    public void setDuracionMinutos(int duracionMinutos) {
+        this.duracionMinutos = duracionMinutos;
+    }
 }
