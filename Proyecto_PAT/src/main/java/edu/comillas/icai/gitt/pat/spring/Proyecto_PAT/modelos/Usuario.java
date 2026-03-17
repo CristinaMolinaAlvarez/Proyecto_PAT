d27a5c57d221b0125056440bf37da92247d1477b
202100539 <202100539@alu.comillas.edu>
2026-03-17 14:25:43 +0100

    Test solucionados y dependencia añadida

--- Proyecto_PAT/src/main/java/edu/comillas/icai/gitt/pat/spring/Proyecto_PAT/modelos/Usuario.java
+++ Proyecto_PAT/src/main/java/edu/comillas/icai/gitt/pat/spring/Proyecto_PAT/modelos/Usuario.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
--- Proyecto_PAT/src/test/java/edu/comillas/icai/gitt/pat/spring/Proyecto_PAT/ReservationsControllerTest.java
+++ Proyecto_PAT/src/test/java/edu/comillas/icai/gitt/pat/spring/Proyecto_PAT/ReservationsControllerTest.java
@@ -3,7 +3,9 @@ package edu.comillas.icai.gitt.pat.spring.Proyecto_PAT;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
